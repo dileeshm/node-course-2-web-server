d6c9479d1a52d7b38b8dd3e80711afe89229a14e
dileeshm <dileeshm@gmail.com>
2018-05-06 20:24:30 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const ejs = require('ejs');
+const fs = require('fs');
+
+const app = new express();
+
+//set view engine to ejs
+app.set('view engine', 'ejs')
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', `${log}\n`, (error) => {
+    if(error) {
+      console.log(`Unable to append to server.log`);
+    }    
+  });
+  next();
+});
+
+// app.use((req, res) => {
+//   res.render('maintenance');
+// });
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my website'    
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    pageTitle: 'About page'    
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Started listening for http requests on port`, 3000);
+});
